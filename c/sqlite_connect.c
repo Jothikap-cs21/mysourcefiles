7f8ea41bbabbab5881a516e607f9e12f88070aab
jothikap-cs21 <jothikap.cs21@kascsathy.ac.in>
2023-08-21 04:43:17 +0000

    first commit

--- c/sqlite_connect.c
+++ c/sqlite_connect.c
@@ -1,10 +1,9 @@
-create table student_details( std_id int,std_name text,std_class text,std_address text);                                                            
+sqlite> create table student_details( std_id int,std_name text,std_class text,std_address text);                                                            
 sqlite> insert into student_details(std_id,std_name,std_class,std_address) values('1','jothika','cs','sathyamangalam');
 sqlite> select *from student_details;
-
+sqlite> .mode box;
 ┌────────┬──────────┬───────────┬────────────────┐
 │ std_id │ std_name │ std_class │  std_address   │
 ├────────┼──────────┼───────────┼────────────────┤
 │ 1      │ jothika  │ cs        │ sathyamangalam │
 └────────┴──────────┴───────────┴────────────────┘
-
